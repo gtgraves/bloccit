c26f71624197fe66d01735bd280c0cb7d095bfe4
George Graves <gtgraves@gmail.com>
2017-05-29 15:41:12 -0500

    Assignment 19 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,16 @@ posts = Post.all
   )
 end
 
+#Create advertisement
+25.times do
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: 25
+  )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} ads created"
