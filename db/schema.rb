7a30a91f5a38f3e6fb00c7616c635ad07ff51348
George Graves <gtgraves@gmail.com>
2017-06-21 17:38:20 -0500

    Checkpoint 28 authorization: completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170618160158) do
+ActiveRecord::Schema.define(version: 20170619220706) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20170618160158) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
